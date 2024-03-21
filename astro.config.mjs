74608da0786a8e756b570928ad069cf9aae5b75c
Louis <louis.morgan@linaro.org>
2024-03-21 11:03:46 +0000

    upgrade package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+ import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind({
+  integrations: [icon(), tailwind({
     applyBaseStyles: false
   }), sitemap()]
 });
